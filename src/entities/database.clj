ec1b92e6cdc5c8a251eb5471ba765107f44a0313
Agustina Barbetta <agustina.barbetta@gmail.com>
2017-09-03 20:10:25 -0300

    Add rule evaluation and tests

--- src/entities/database.clj
+++ src/entities/database.clj
@@ -1,10 +1,36 @@
-(ns entities.database)
+(ns entities.database
+  (:require [entities.rule]))
 
 (defprotocol Compliant
-  (complies-with-fact [this query]))
+  (complies-with-fact [this query])
+  (complies-with-rule [this query])
+  )
+
+(defn fact-query
+  ""
+  [database query]
+  (some (fn [fact] (= (:sentence fact) query)) (:facts database))
+  )
+
+(defn rule-query
+  ""
+  [database rule query]
+  (every? (fn [x] (fact-query database x)) (entities.rule/evaluate rule query))
+  )
+
+(defn find-rule
+  ""
+  [database query]
+  (first (filter (fn [rule] (= (:predicate (:signature rule)) query)) (:rules database)))
+  )
 
 (defrecord DataBase [facts rules malformations]
   Compliant
   (complies-with-fact [this query]
-                      (some (fn [fact] (= (:sentence fact) query)) (:facts this)))
+                      (fact-query this query))
+  (complies-with-rule [this query]
+                      (if-let [rule (find-rule this query)]
+                        (rule-query this rule query)
+                        false)
+                      )
   )
