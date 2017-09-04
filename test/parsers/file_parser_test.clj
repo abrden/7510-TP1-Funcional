f3f336bd600a480346a13d99833bb95644587d22
Agustina Barbetta <agustina.barbetta@gmail.com>
2017-09-04 01:01:05 -0300

    Fix file parser bug

--- test/parsers/file_parser_test.clj
+++ test/parsers/file_parser_test.clj
@@ -4,6 +4,7 @@
 
 (def FACTS_FILE "test/files/facts.txt")
 (def MALFORMED_FILE "test/files/malformed.txt")
+(def NUMBERS_FILE "test/files/number_database.txt")
 
 (deftest simple-file-parse
   (testing "Tests a simple file parse"
@@ -12,3 +13,8 @@
 (deftest malformed-file-parse
   (testing "Tests a malformed file parse"
            (is (= (count (:malformations (parse-file MALFORMED_FILE))) 3))))
+
+(deftest number-file-parse
+  (testing "Tests a number file parse"
+    (let [db (parse-file NUMBERS_FILE)]
+      (is (and (= (count (:rules db)) 1) (= (count (:facts db)) 9) (= (count (:malformations db)) 0))))))
