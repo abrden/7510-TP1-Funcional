58e6500aba72a097255d65c4e0a8200629985c4c
Agustina Barbetta <agustina.barbetta@gmail.com>
2017-09-10 15:16:14 -0300

    Sentence formatter documentation

--- src/formatters/sentence_formatter.clj
+++ src/formatters/sentence_formatter.clj
@@ -2,7 +2,8 @@
   (:require [clojure.string :as str]))
 
 (defn format-sentence
-  ""
+  "Receives a string predicate and a sequence of args.
+  Returns a string sentence."
   [predicate args]
   (str predicate "(" (str/join ", " args) ").")
   )
