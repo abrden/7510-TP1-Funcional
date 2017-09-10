d5e4b3092051be128af32f0da915513e7b4c9ef9
Agustina Barbetta <agustina.barbetta@gmail.com>
2017-09-10 15:24:25 -0300

    File parser documentation

--- src/parsers/file_parser.clj
+++ src/parsers/file_parser.clj
@@ -5,7 +5,9 @@
   )
 
 (defn dispatch-parser
-  ""
+  "Receives a string sentence from the database file.
+  Dispatches the fact or rule parser.
+  Returns a Fact, Rule or Malformation record depending on the sentence format."
   [sentence]
   (cond
     (re-matches #".+\(.+\)\ *:-\ *.*\." sentence) (parsers.rule-parser/parse-rule sentence)
@@ -14,7 +16,8 @@
   )
 
 (defn parse-file
-  ""
+  "Receibes a string representing the database file name.
+  Returns a DataBase record containing the facts, rules and malformations of the file."
   [fileName]
   (let [ dbmap (group-by type
           (map dispatch-parser
--- test/parsers/file_parser_test.clj
+++ test/parsers/file_parser_test.clj
@@ -2,25 +2,25 @@
   (:require [clojure.test :refer :all]
             [parsers.file-parser :refer :all]))
 
-(def FACTS_FILE "test/files/facts.txt")
-(def MALFORMED_FILE "test/files/malformed.txt")
-(def NUMBERS_FILE "test/files/number_database.txt")
-(def WITHOUT_DOTS_FILE "test/files/without_dots.txt")
+(def facts-file "test/files/facts.txt")
+(def malformed-file "test/files/malformed.txt")
+(def numbers-file "test/files/number_database.txt")
+(def without-dots-file "test/files/without_dots.txt")
 
 (deftest simple-file-parse
   (testing "Tests a simple file parse"
-    (is (= (count (:facts (parse-file FACTS_FILE))) 3))))
+    (is (= (count (:facts (parse-file facts-file))) 3))))
 
 (deftest malformed-file-parse
   (testing "Tests a malformed file parse"
-           (is (= (count (:malformations (parse-file MALFORMED_FILE))) 3))))
+    (is (= (count (:malformations (parse-file malformed-file))) 3))))
 
 (deftest number-file-parse
   (testing "Tests a number file parse"
-    (let [db (parse-file NUMBERS_FILE)]
+    (let [db (parse-file numbers-file)]
       (is (and (= (count (:rules db)) 1) (= (count (:facts db)) 9) (= (count (:malformations db)) 0))))))
 
 (deftest db-without-dot-ending-is-malformed
   (testing "Tests a file without dots parse"
-    (let [db (parse-file WITHOUT_DOTS_FILE)]
+    (let [db (parse-file without-dots-file)]
       (is (and (= (count (:rules db)) 0) (= (count (:facts db)) 0) (= (count (:malformations db)) 3))))))
